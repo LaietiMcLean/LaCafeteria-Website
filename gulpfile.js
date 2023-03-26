110fd9ab97ecfa53272d8561ab76763066da5ed7
Laiamdelprado <lmarcodelprado@gmail.com>
2023-03-26 21:45:10 +0200

    style(): add base folder with basic utilities, first scss routing and some modifications at the gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,21 @@
-const { src, dest } = require('gulp');
+const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
 
 function css(done) {
     src('src/scss/app.scss')
-    .pipe(sass())
+    .pipe(sass({outputStyle: 'expanded'}))
+    .pipe(postcss([autoprefixer()]))
     .pipe(dest('build/css'));
 
     done();
 }
 
+function dev() {
+    watch('src/scss/**/*.scss', css);
+}
+
 exports.css = css;
+exports.dev = dev;
+exports.default = series(css, dev)
